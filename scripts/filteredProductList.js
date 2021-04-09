37d78d096b56880d47c7bdc0684dbd2317ff325d
Kat <zur.katarzyna@gmail.com>
2021-04-09 15:43:06 +0100

    Create filteredProductList class

---
+++ scripts/filteredProductList.js
@@ -0,0 +1,36 @@
+// const classMdlProduct = require('./product.js');
+
+class FilteredProductList {
+	constructor() {
+		this._arrProducts = [];
+	}
+
+	getFilteredProducts() {
+		return this._arrProducts;
+	}
+
+	listAllFilteredProducts() {
+		const arrAllProducts = this.getFilteredProducts();
+		for (let i = 0; i < arrAllProducts.length; i++) {
+			const objProduct = arrAllProducts[i];
+			console.log(objProduct.toString());
+		}
+	}
+
+	addProductToFilteredList(objProduct) {
+		let arrItems = this.getFilteredProductList();
+		arrItems.push(objProduct);
+	}
+
+	getItem(intId) {
+		let arrItems = this.getFilteredProducts();
+		for (let i = 0; i < arrItems.length; i++) {
+			const intProductId = arrItems[i].getId();
+			if (intId === intProductId) {
+				return arrItems[i];
+			}
+		}
+	}
+}
+
+module.exports = FilteredProductList;
