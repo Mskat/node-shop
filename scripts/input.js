6d3f304691d2620ba03b635bdf96d73d1992ac0f
Kat <zur.katarzyna@gmail.com>
2021-04-09 15:02:03 +0100

    Create Input class

---
+++ scripts/input.js
@@ -0,0 +1,13 @@
+const prompt = require('prompt-sync')({ sigint: true });
+
+class Input {
+	static _getInput() {
+		let strInput = prompt('')
+			.trim()
+			.toLowerCase();
+            
+		return strInput;
+	}
+}
+
+module.exports = Input;
