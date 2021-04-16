7bb97d3712651312658b89710aff8b8cb9d9e8a2
Kat <zur.katarzyna@gmail.com>
2021-04-16 15:02:22 +0100

    Delete hard-coded array of items

--- scripts/productList.js
+++ scripts/productList.js
@@ -1,37 +1,5 @@
-const classMdlProduct = require('../scripts/product.js');
-
 class ProductList {
 	constructor() {
-		this._arrProducts = [
-			new classMdlProduct(
-				1,
-				'Pillow',
-				'https://zvelo.com/wp-content/tld-path-protocol.jpg',
-				34.99,
-				'Some description about a Pillow.',
-			),
-			new classMdlProduct(
-				2,
-				'Matress',
-				'https://www.wikihow.com/images/he-URL-for-Step-2-6.jpg.webp',
-				129.99,
-				'Some description about a Matress.',
-			),
-			new classMdlProduct(
-				3,
-				'Classic Pillow',
-				'https://some.com/wp-kmk/tld-path.jpg',
-				64.99,
-				'Some description about a Classic Pillow.',
-			),
-			new classMdlProduct(
-				4,
-				'Bookcase',
-				'https://media.wired.com/photos/5lling%20the%20URL.jpg',
-				229.00,
-				'Some description about a Bookcase.',
-			),
-		];
 	}
 
 	getProducts() {
